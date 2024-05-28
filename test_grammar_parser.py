91b7c94f4ff82ba3d19298f802145b684f1af195
CamsMars <125709997+CamsMars@users.noreply.github.com>
2024-05-27 19:27:13 -0500

    actualizacion unittest

--- test_grammar_parser.py
+++ test_grammar_parser.py
@@ -1,42 +1,57 @@
 import unittest
-from main import compute_first_and_follow_sets
+from main import compute_first, compute_first_string, compute_follow
+from collections import defaultdict
 
 class TestGrammarParser(unittest.TestCase):
     def read_grammar(self, filename):
         with open(filename, 'r') as file:
             return file.read()
     
-    def test_grammar_1(self):
-        grammar = self.read_grammar('grammars/grammar1.txt')
-        first, follow = compute_first_and_follow_sets(grammar)
+    def test_compute_first(self):
+        productions = {
+            'S': [['A', 'B']],
+            'A': [['a'], ['e']],
+            'B': [['b']]
+        }
+        first = defaultdict(set)
+        visited = set()
+        compute_first('S', productions, first, visited)
         self.assertEqual(first['S'], {'a', 'b', 'e'})
-        self.assertEqual(follow['S'], {'$'})
+        compute_first('A', productions, first, visited)
         self.assertEqual(first['A'], {'a', 'e'})
-        self.assertEqual(follow['A'], {'b'})
+        compute_first('B', productions, first, visited)
         self.assertEqual(first['B'], {'b'})
-        self.assertEqual(follow['B'], {'$', 'a'})
 
-    def test_grammar_2(self):
-        grammar = self.read_grammar('grammars/grammar2.txt')
-        first, follow = compute_first_and_follow_sets(grammar)
-        self.assertEqual(first['S'], {'a', 'd', 'b', 'c', 'e'})
+    def test_compute_first_string(self):
+        productions = {
+            'S': [['A', 'B']],
+            'A': [['a'], ['e']],
+            'B': [['b']]
+        }
+        first = defaultdict(set)
+        visited = set()
+        for non_terminal in productions:
+            compute_first(non_terminal, productions, first, visited)
+        result = compute_first_string(['A', 'B'], productions, first)
+        self.assertEqual(result, {'a', 'b', 'e'})
+
+    def test_compute_follow(self):
+        productions = {
+            'S': [['A', 'B']],
+            'A': [['a'], ['e']],
+            'B': [['b']]
+        }
+        first = defaultdict(set)
+        follow = defaultdict(set)
+        visited = set()
+        for non_terminal in productions:
+            compute_first(non_terminal, productions, first, visited)
+        compute_follow('S', productions, first, follow, 'S')
         self.assertEqual(follow['S'], {'$'})
-        self.assertEqual(first['A'], {'a', 'e'})
+        compute_follow('A', productions, first, follow, 'S')
         self.assertEqual(follow['A'], {'b'})
-        self.assertEqual(first['B'], {'d'})
-        self.assertEqual(follow['B'], {'c'})
-    
-    def test_grammar_3(self):
-        grammar = self.read_grammar('grammars/grammar3.txt')
-        first, follow = compute_first_and_follow_sets(grammar)
-        self.assertEqual(first['S'], {'a', 'b', 'c', 'e'})
-        self.assertEqual(follow['S'], {'$'})
-        self.assertEqual(first['A'], {'a', 'e'})
-        self.assertEqual(follow['A'], {'b', 'c'})
-        self.assertEqual(first['B'], {'b', 'e'})
-        self.assertEqual(follow['B'], {'c'})
-        self.assertEqual(first['C'], {'c'})
-        self.assertEqual(follow['C'], {'$', 'a'})
+        compute_follow('B', productions, first, follow, 'S')
+        self.assertEqual(follow['B'], {'$', 'a'})
 
 if __name__ == '__main__':
     unittest.main()
